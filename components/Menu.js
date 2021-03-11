7489a6a1bc9e77594e83ca006340b470262e71a1
Alex Edwards <alexedwards@Alexs-MacBook-Pro.local>
2021-03-10 17:35:57 -0700

    finished mvp

--- components/Menu.js
+++ components/Menu.js
@@ -1,12 +1,12 @@
 // This is the data we will be using, study it but don't change anything, yet.
 
 let menuItems = [
-  'Students',
-  'Faculty',
+  "Students",
+  "Faculty",
   "What's New",
-  'Tech Trends',
-  'Music',
-  'Log Out'
+  "Tech Trends",
+  "Music",
+  "Log Out",
 ];
 
 /* 
@@ -31,3 +31,30 @@ let menuItems = [
 
   Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
 */
+
+function menuMaker(array) {
+  const menu = document.createElement("div");
+  const unlist = document.createElement("ul");
+
+  menu.classList.add("menu");
+
+  menu.appendChild(unlist);
+
+  for (let i = 0; i < array.length; i++) {
+    const list = document.createElement("li");
+    unlist.appendChild(list);
+    list.textContent = array[i];
+  }
+  const menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", function (event) {
+    menu.classList.toggle("menu--open");
+  });
+
+  return menu;
+}
+
+const headerMenu = menuMaker(menuItems);
+const header = document.querySelector(".header");
+header.appendChild(headerMenu);
+
+console.log(menuMaker(menuItems));
